5e580903d3ea2cc3105c96e1719bc20ff343e0f7
Austin Lee <wcnhl1@gmail.com>
2017-02-05 22:06:53 -0800

    adding Stickman.java

---
+++ Stickman.java
@@ -0,0 +1,41 @@
+package q1;
+
+
+/**
+ * This will draw a stick man using the String class.
+ * I will create a continuous shape using the println method.
+ *
+ * @author Austin Lee
+ * @version 1.0
+ */
+public class Stickman {
+    /**
+     * This is the main method (entry point) that gets called by the JVM.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(String[] args) {
+        // your code will go here!!!        
+        System.out.println("Question one was called and ran sucessfully.");
+        System.out.println("");
+        
+        // Stickman
+        System.out.println("\t   \t\\/");
+        System.out.println("\t       \"\"\"\"\"\"\"\"");
+        System.out.println("\t       \"\"\"\"\"\" \"");
+        System.out.println("\t       \"• < • \"");
+        System.out.println("\t       \"  -   \"");
+        System.out.println("\t        \\____/");
+        System.out.println("\t     ____| |____     ");
+        System.out.println("\t    /    '-'    \\  ");
+        System.out.println("\t   |      ©      |");
+        System.out.println("\t   |_|\\       /|_|");
+        System.out.println("\t    0  |==w==|  0");
+        System.out.println("\t       /     \\   ");
+        System.out.println("\t      |   |   |   ");
+        System.out.println("\t      |   |   |   ");
+        System.out.println("\t      |___|___|   ");
+        System.out.println("\t     /////|\\\\\\\\\\   ");
+        
+    }
+}
